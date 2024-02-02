ff904fcee13b0473b0b882c527e2af96980dc2b5
talatkuyuk <talatkuyuk@gmail.com>
2024-02-02 15:28:50 +0300

    added the test

---
+++ tests/test.spec.ts
@@ -0,0 +1,113 @@
+import { compile } from "@mdx-js/mdx";
+import dedent from "dedent";
+
+import recmaEscapeMissingComponents, { type TestFunction } from "../src";
+
+describe("without the plugin", () => {
+  // ******************************************
+  it("sets no default value for the components (the default behaviour)", async () => {
+    const source = dedent`
+      # Hi.
+      
+      <Component1 />
+      
+      Wellcome.
+      
+      <Component2 />
+    `;
+
+    const compiledSource = await compile(source);
+
+    expect(String(compiledSource)).toContain("{Component1, Component2} = _components;");
+  });
+});
+
+describe("with the plugin (no option)", () => {
+  // ******************************************
+  it("sets the default value '() => null' for all components", async () => {
+    const source = dedent`
+      # Hi.
+      
+      <Component1 />
+      
+      Wellcome.
+      
+      <Component2 />
+    `;
+
+    const compiledSource = await compile(source, {
+      recmaPlugins: [recmaEscapeMissingComponents],
+    });
+
+    expect(String(compiledSource)).toContain(
+      "{Component1 = () => null, Component2 = () => null} = _components;",
+    );
+  });
+});
+
+describe("with the plugin (has `test` option)", () => {
+  // ******************************************
+  it("sets the default value '() => null' for the component passes the test (string)", async () => {
+    const source = dedent`
+      # Hi.
+      
+      <Component1 />
+      
+      Wellcome.
+      
+      <Component2 />
+    `;
+
+    const compiledSource = await compile(source, {
+      recmaPlugins: [[recmaEscapeMissingComponents, "Component1"]],
+    });
+
+    expect(String(compiledSource)).toContain(
+      "{Component1 = () => null, Component2} = _components;",
+    );
+  });
+
+  // ******************************************
+  it("sets the default value '() => null' for the components pass the test (string array)", async () => {
+    const source = dedent`
+      # Hi.
+      
+      <Component1 />
+      
+      Wellcome.
+      
+      <Component2 />
+    `;
+
+    const compiledSource = await compile(source, {
+      recmaPlugins: [[recmaEscapeMissingComponents, ["Component1"]]],
+    });
+
+    expect(String(compiledSource)).toContain(
+      "{Component1 = () => null, Component2} = _components;",
+    );
+  });
+
+  // ******************************************
+  it("sets the default value '() => null' for the components pass the test (function)", async () => {
+    const source = dedent`
+      # Hi.
+      
+      <Component1 />
+      
+      Wellcome.
+      
+      <Component2 />
+    `;
+
+    const compiledSource = await compile(source, {
+      recmaPlugins: [
+        [recmaEscapeMissingComponents, ((name) => name.endsWith("2")) as TestFunction],
+      ],
+    });
+
+    expect(String(compiledSource)).toContain(
+      "{Component1, Component2 = () => null} = _components;",
+    );
+  });
+});
